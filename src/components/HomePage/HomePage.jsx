08faa5acdd91c9ed30b2971d2d659699a60a77fd
Ceydaay <cydylmz.99@gmail.com>
2025-02-23 23:57:46 +0300

    first commit

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>📚 Library Management System 📚</h1>
+      <p className="description">
+        Welcome to the Library Management System. This tool is designed to help you organize, manage, and track your library’s books, authors, and categories. Whether you are a librarian or a book enthusiast, this system will make library management easier and more efficient.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
